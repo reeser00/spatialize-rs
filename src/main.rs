bf4c7050e4991fabd68fd036fe2fb84791dadf8e
reeser00 <neumann.marvin00@gmail.com>
2024-10-04 14:27:32 +0200

    quadtree documentation

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,8 @@
-use data_structures_rs::quadtree;
+use data_structures_rs::quadtree::{Quadtree, Sized};
 use std::rc::Rc;
-use nannou::prelude::*;
 
-const QT_SIZE: f32 = 400.0;
+fn main() {}
 
-fn main() {
-    nannou::sketch(view).run();
-}
-
-//NANNOU
-fn view(app: &App, frame: Frame) {
-    let mut qt = quadtree::Quadtree::new(QT_SIZE * -0.5, QT_SIZE * 0.5, QT_SIZE, QT_SIZE);
-    let rect = Rectangle::new(1.0, 1.0, 2.0, 2.0);
-    match qt.insert(Rc::new(rect)) {
-        Ok(_) => {
-            let mut vec: Vec<Rc<dyn quadtree::Sized>> = vec![];
-            let rect = Rectangle::new(190.0, 190.0, 2.0, 2.0);
-            let _ = qt.insert(Rc::new(rect));
-            let rect: Rc<dyn quadtree::Sized> = Rc::new(Rectangle::new(QT_SIZE * -0.5 + 1.0, QT_SIZE * 0.5 - 1.0, QT_SIZE * 0.9, QT_SIZE * 0.9));
-            let r = qt.get_rect(Rc::clone(&rect), &mut vec);
-            println!("{:?} - {:?}", r, vec);
-
-            let draw = app.draw();
-            draw.background().rgb(0.0, 0.0, 0.0);
-            draw.to_frame(app, &frame).unwrap();
-        },
-        Err(e) => eprintln!("{}", e),
-    }
-}
-
-// QUADTREE
 #[derive(Debug)]
 struct Rectangle {
     position_x: f32,
@@ -49,7 +22,7 @@ impl Rectangle {
     }
 }
 
-impl quadtree::Sized for Rectangle {
+impl Sized for Rectangle {
     fn north_edge(&self) -> f32 {
         self.position_y
     }
--- src/quadtree.rs
+++ src/quadtree.rs
@@ -1,7 +1,9 @@
-use std::rc::Rc;
 use std::cell::RefCell;
 use std::fmt::Debug;
+use std::rc::Rc;
 
+/// A recursive data structure that divides a two-dimensional space into quadrants,
+/// used for efficient spatial partitioning of elements positioned in a 2D space.
 #[derive(Debug)]
 pub struct Quadtree {
     position_x: f32,
@@ -16,6 +18,38 @@ pub struct Quadtree {
     contents: Vec<Rc<dyn Sized>>,
 }
 
+/// The `Sized` trait defines four functions `north_edge()`, `east_edge()`, `south_edge()`, `west_edge()`
+/// each returning the `f32` position of the respective edge.
+/// Implementing this trait is required to insert elements into the `Quadtree`, as it provides the boundaries
+/// for spatial partitioning.
+///
+/// # Examples
+/// ```
+/// struct Rectangle {
+///     position_x: f32,
+///     position_y: f32,
+///     width: f32,
+///     height: f32,
+/// }
+///
+/// impl Sized for Rectangle {
+///     fn north_edge(&self) -> f32 {
+///         self.position_y
+///     }
+///
+///     fn east_edge(&self) -> f32 {
+///         self.position_x + self.width
+///     }
+///
+///     fn south_edge(&self) -> f32 {
+///         self.position_y - self.height
+///     }
+///
+///     fn west_edge(&self) -> f32 {
+///         self.position_x
+///     }
+/// }
+/// ```
 pub trait Sized: Debug {
     fn north_edge(&self) -> f32;
     fn east_edge(&self) -> f32;
@@ -24,6 +58,17 @@ pub trait Sized: Debug {
 }
 
 impl Quadtree {
+    /// Returns a `Quadtree` with the specified boundaries.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let position_x: f32 = -100.0;
+    /// let position_y: f32 = 100.0;
+    /// let width: f32 = 200.0;
+    /// let height: f32 = 200.0;
+    /// let qt = Quadtree::new(position_x, position_y, width, height);
+    /// ```
     pub fn new(position_x: f32, position_y: f32, width: f32, height: f32) -> Self {
         Self {
             position_x,
@@ -39,80 +84,139 @@ impl Quadtree {
         }
     }
 
+    /// A private function used to partition the `Quadtree` into four quadrants.
     fn subdivide(&mut self) {
         if !self.divided {
-            self.northeast_quad = Some(Rc::new(RefCell::new(Quadtree::new(self.position_x + self.width / 2.0, self.position_y, self.width / 2.0, self.height / 2.0))));
-            self.northwest_quad = Some(Rc::new(RefCell::new(Quadtree::new(self.position_x, self.position_y, self.width / 2.0, self.height / 2.0))));
-            self.southeast_quad = Some(Rc::new(RefCell::new(Quadtree::new(self.position_x + self.width / 2.0, self.position_y - self.height / 2.0, self.width / 2.0, self.height / 2.0))));
-            self.southwest_quad = Some(Rc::new(RefCell::new(Quadtree::new(self.position_x, self.position_y - self.height / 2.0, self.width / 2.0, self.height / 2.0))));
+            self.northeast_quad = Some(Rc::new(RefCell::new(Quadtree::new(
+                self.position_x + self.width / 2.0,
+                self.position_y,
+                self.width / 2.0,
+                self.height / 2.0,
+            ))));
+            self.northwest_quad = Some(Rc::new(RefCell::new(Quadtree::new(
+                self.position_x,
+                self.position_y,
+                self.width / 2.0,
+                self.height / 2.0,
+            ))));
+            self.southeast_quad = Some(Rc::new(RefCell::new(Quadtree::new(
+                self.position_x + self.width / 2.0,
+                self.position_y - self.height / 2.0,
+                self.width / 2.0,
+                self.height / 2.0,
+            ))));
+            self.southwest_quad = Some(Rc::new(RefCell::new(Quadtree::new(
+                self.position_x,
+                self.position_y - self.height / 2.0,
+                self.width / 2.0,
+                self.height / 2.0,
+            ))));
             self.divided = true;
         }
     }
 
+    /// Inserts an object implementing the `Sized` trait.
+    ///
+    /// # Examples
+    /// ```
+    /// let mut qt = Quadtree::new(-10.0, 10.0, 20.0, 20.0);
+    /// let sized_object: Rc<dyn Sized> = Rc::new(Rectangle::new(0.0, 0.0, 5.0, 5.0));
+    /// match qt.insert(Rc::clone(&sized_object)) {
+    ///     Ok(_) => (),
+    ///     Err(e) => eprintln!("{}", e),
+    /// }
+    /// ```
     pub fn insert(&mut self, sized_object: Rc<dyn Sized>) -> Result<(), String> {
-        if sized_object.north_edge() <= self.position_y &&
-            sized_object.east_edge() <= self.position_x + self.width &&
-            sized_object.south_edge() >= self.position_y - self.height &&
-            sized_object.west_edge() >= self.position_x {
+        if sized_object.north_edge() <= self.position_y
+            && sized_object.east_edge() <= self.position_x + self.width
+            && sized_object.south_edge() >= self.position_y - self.height
+            && sized_object.west_edge() >= self.position_x
+        {
+            //Object fits in Quadtree
+            if !self.divided {
+                self.subdivide();
+            }
+            if let Some(rc_ref) = &self.northeast_quad {
+                if let Ok(_) = rc_ref.borrow_mut().insert(Rc::clone(&sized_object)) {
+                    return Ok(());
+                }
+            }
+            if let Some(rc_ref) = &self.northwest_quad {
+                if let Ok(_) = rc_ref.borrow_mut().insert(Rc::clone(&sized_object)) {
+                    return Ok(());
+                }
+            }
+            if let Some(rc_ref) = &self.southeast_quad {
+                if let Ok(_) = rc_ref.borrow_mut().insert(Rc::clone(&sized_object)) {
+                    return Ok(());
+                }
+            }
+            if let Some(rc_ref) = &self.southwest_quad {
+                if let Ok(_) = rc_ref.borrow_mut().insert(Rc::clone(&sized_object)) {
+                    return Ok(());
+                }
+            }
 
-                //Object fits in Quadtree
-                if !self.divided {
-                    self.subdivide();
-                }
-                if let Some(rc_ref) = &self.northeast_quad {
-                    if let Ok(_) = rc_ref.borrow_mut().insert(Rc::clone(&sized_object)) {
-                        return Ok(());
-                    }
-                }
-                if let Some(rc_ref) = &self.northwest_quad {
-                    if let Ok(_) = rc_ref.borrow_mut().insert(Rc::clone(&sized_object)) {
-                        return Ok(());
-                    }
-                }
-                if let Some(rc_ref) = &self.southeast_quad {
-                    if let Ok(_) = rc_ref.borrow_mut().insert(Rc::clone(&sized_object)) {
-                        return Ok(());
-                    }
-                }
-                if let Some(rc_ref) = &self.southwest_quad {
-                    if let Ok(_) = rc_ref.borrow_mut().insert(Rc::clone(&sized_object)) {
-                        return Ok(());
-                    }
-                }
-
-                //Object doesn't fit in any divisions
-                self.contents.push(sized_object);
-                Ok(())
+            //Object doesn't fit in any divisions
+            self.contents.push(sized_object);
+            Ok(())
         } else {
-            Err(String::from("Object doesn't fit within the Quadtree bounds."))
+            Err(String::from(
+                "Object doesn't fit within the Quadtree bounds.",
+            ))
         }
     }
 
-    pub fn get_rect(&self, rect: Rc<dyn Sized>, vec: &mut Vec<Rc<dyn Sized>>) -> Result<(), String> {
-        if !(rect.north_edge() < self.position_y - self.height ||
-            rect.east_edge() < self.position_x ||
-            rect.south_edge() > self.position_y ||
-            rect.west_edge() > self.position_x + self.width) {
-                if self.divided {
-                    if let Some(rc_ref) = &self.northeast_quad {
-                        let _ = rc_ref.borrow().get_rect(Rc::clone(&rect), vec);
-                    }
-                    if let Some(rc_ref) = &self.northwest_quad {
-                        let _ = rc_ref.borrow().get_rect(Rc::clone(&rect), vec);
-                    }
-                    if let Some(rc_ref) = &self.southeast_quad {
-                        let _ = rc_ref.borrow().get_rect(Rc::clone(&rect), vec);
-                    }
-                    if let Some(rc_ref) = &self.southwest_quad {
-                        let _ = rc_ref.borrow().get_rect(Rc::clone(&rect), vec);
-                    }
+    /// Searches the `Quadtree` using a two-dimensional view that implementing `Sized`
+    ///
+    /// # Examples
+    /// ```
+    /// let mut qt = Quadtree::new(-10.0, 10.0, 20.0, 20.0);
+    /// let sized_object: Rc<dyn Sized> = Rc::new(Rectangle::new(0.0, 0.0, 5.0, 5.0));
+    /// match qt.insert(Rc::clone(&sized_object)) {
+    ///     Ok(_) => {
+    ///         let rect_view: Rc<dyn Sized> = Rc::new(Rectangle::new(-2, 2, 10.0, 10.0));
+    ///         let mut result_vec: Vec<Rc<dyn Sized>> = vec![];
+    ///         match qt.get_rect(rect_view, &mut result_vec) {
+    ///             Ok(_) => assert_eq!(1, result_vec.len()),
+    ///             Err(e) => eprintln!("{}", e),
+    ///         }
+    ///     },
+    ///     Err(e) => eprintln!("{}", e),
+    /// }
+    /// ```
+    pub fn get_rect(
+        &self,
+        rect: Rc<dyn Sized>,
+        vec: &mut Vec<Rc<dyn Sized>>,
+    ) -> Result<(), String> {
+        if !(rect.north_edge() < self.position_y - self.height
+            || rect.east_edge() < self.position_x
+            || rect.south_edge() > self.position_y
+            || rect.west_edge() > self.position_x + self.width)
+        {
+            if self.divided {
+                if let Some(rc_ref) = &self.northeast_quad {
+                    let _ = rc_ref.borrow().get_rect(Rc::clone(&rect), vec);
                 }
-                for rc in self.contents.iter() {
-                    vec.push(Rc::clone(&rc));
+                if let Some(rc_ref) = &self.northwest_quad {
+                    let _ = rc_ref.borrow().get_rect(Rc::clone(&rect), vec);
                 }
-                Ok(())
+                if let Some(rc_ref) = &self.southeast_quad {
+                    let _ = rc_ref.borrow().get_rect(Rc::clone(&rect), vec);
+                }
+                if let Some(rc_ref) = &self.southwest_quad {
+                    let _ = rc_ref.borrow().get_rect(Rc::clone(&rect), vec);
+                }
+            }
+            for rc in self.contents.iter() {
+                vec.push(Rc::clone(&rc));
+            }
+            Ok(())
         } else {
-            Err(String::from("Rectangle doesn't overlap the Quadtree bounds."))
+            Err(String::from(
+                "Rectangle doesn't overlap the Quadtree bounds.",
+            ))
         }
     }
 }
